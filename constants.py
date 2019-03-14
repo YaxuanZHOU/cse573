1c2fb64b513f3e173b72dd3157ad4498ad8d84ac
yaxuan <yaxuanzh@uw.edu>
2019-03-14 05:40:02 -0700

    test

---
+++ constants.py
@@ -0,0 +1,31 @@
+
+MOVE_AHEAD = 'MoveAhead'
+ROTATE_LEFT = 'RotateLeft'
+ROTATE_RIGHT = 'RotateRight'
+LOOK_UP = 'LookUp'
+LOOK_DOWN = 'LookDown'
+#DONE_T = 0  # YZ
+#DONE_B = 1   # YZ
+# DONE_ALL = 2   # YZ
+FOUND_T = 'FoundTomato'
+FOUND_M = 'FoundMicrowave'
+
+BASIC_ACTIONS = [MOVE_AHEAD, ROTATE_LEFT, ROTATE_RIGHT, LOOK_UP, LOOK_DOWN, FOUND_T, FOUND_M]     # YZ
+
+# YZ: VANILLA REWARD
+# GOAL_SUCCESS_REWARD = 5
+# STEP_PENALTY = -0.01
+# FAILED_ACTION_PENALTY = 0
+
+# YZ: REWARD CHANGE 1
+# GOAL2_SUCCESS_REWARD = 30   # reward for finding another object
+# GOAL_SUCCESS_REWARD = 5     # reward for finding one object
+# STEP_PENALTY = -0.01
+# FAILED_ACTION_PENALTY = 0
+
+# YZ: REWARD CHANGE 2
+GOAL2_SUCCESS_REWARD = 15   # reward for finding another object
+GOAL_SUCCESS_REWARD = 5     # reward for finding one object
+STEP_PENALTY = -0.01
+FAILED_ACTION_PENALTY = -0.1 # penalty for failed action
+
---
+++ episode.py
@@ -0,0 +1,125 @@
+""" Contains the Episodes for Navigation. """
+import random
+import torch
+import time
+import sys
+from constants import GOAL_SUCCESS_REWARD, STEP_PENALTY, BASIC_ACTIONS, GOAL2_SUCCESS_REWARD, \
+    FAILED_ACTION_PENALTY  # YZ
+from environment import Environment
+from utils.net_util import gpuify
+import numpy as np
+
+
+class Episode:
+    """ Episode for Navigation. """
+
+    def __init__(self, args, gpu_id, rank, strict_done=False):
+        super(Episode, self).__init__()
+
+        self._env = None
+
+        self.gpu_id = gpu_id
+        self.strict_done = strict_done
+        self.task_data = None
+        self.glove_embedding = None
+
+        self.seed = args.seed + rank
+        random.seed(self.seed)
+
+        with open('./datasets/objects/int_objects.txt') as f:
+            int_objects = [s.strip() for s in f.readlines()]
+        with open('./datasets/objects/rec_objects.txt') as f:
+            rec_objects = [s.strip() for s in f.readlines()]
+
+        self.objects = int_objects + rec_objects
+
+        self.actions_list = [{'action': a} for a in BASIC_ACTIONS]
+        self.actions_taken = []
+
+    @property
+    def environment(self):
+        return self._env
+
+    def state_for_agent(self):
+        # YZ: add network-found memory into the state space.
+        memory = list(np.array(self.envSuccess).astype(float))
+        return [self.environment.current_frame, memory]
+
+    def step(self, action_as_int):
+        action = self.actions_list[action_as_int]
+        self.actions_taken.append(action)
+        return self.action_step(action)
+
+    def action_step(self, action):
+        self.environment.step(action)
+        reward, terminal, action_was_successful = self.judge(action)
+
+        return reward, terminal, action_was_successful
+
+    def slow_replay(self, delay=0.2):
+        # Reset the episode
+        self._env.reset(self.cur_scene, change_seed=False)
+
+        for action in self.actions_taken:
+            self.action_step(action)
+            time.sleep(delay)
+
+    def judge(self, action):
+        """ Judge the last event. """
+        # immediate reward
+        reward = STEP_PENALTY
+        done = False
+        action_was_successful = self.environment.last_action_success
+        if action['action'] == 'FoundTomato':
+            objects = self._env.last_event.metadata['objects']
+            visible_objects = {o['objectType']: o['objectId'] for o in objects if o['visible']}
+            if 'Tomato' in list(visible_objects.keys()) and self.envSuccess[0] == False:
+                self.tomatoID = visible_objects['Tomato']
+                tomatoSuccess = self.environment.tomatoEnvSuccess(self.tomatoID)
+                if tomatoSuccess:
+                    self.envSuccess[0] == True
+                    reward += GOAL2_SUCCESS_REWARD
+
+        if action['action'] == 'FoundMicrowave':
+            objects = self._env.last_event.metadata['objects']
+            visible_objects = {o['objectType']: o['objectId'] for o in objects if o['visible']}
+            if 'Microwave' in list(visible_objects.keys()) and self.envSuccess[1] == False:
+                microwaveSuccess = self.environment.microwaveEnvSuccess(visible_objects['Microwave'], self.tomatoID)
+                if microwaveSuccess:
+                    self.envSuccess[1] = True
+                    reward += GOAL_SUCCESS_REWARD
+
+        if action['action'] == 'Done':
+            done = True
+            self.success = self.envSuccess[0] and self.envSuccess[0]
+            if self.success:
+                reward += GOAL_SUCCESS_REWARD
+
+        return reward, done, action_was_successful
+
+    def new_episode(self, args, scene):
+
+        if self._env is None:
+            if args.arch == 'osx':
+                local_executable_path = './datasets/builds/thor-local-OSXIntel64.app/Contents/MacOS/thor-local-OSXIntel64'
+            else:
+                local_executable_path = './datasets/builds/thor-local-Linux64'
+
+            self._env = Environment(
+                grid_size=args.grid_size,
+                fov=args.fov,
+                local_executable_path=local_executable_path,
+                randomize_objects=args.randomize_objects,
+                seed=self.seed)
+            self._env.start(scene, self.gpu_id)
+        else:
+            self._env.reset(scene)
+
+        # For now, single target.
+        self.envSuccess = [False, False]
+        self.tomatoID = 0.
+        self.success = False
+        self.cur_scene = scene
+        self.actions_taken = []
+
+        return True
